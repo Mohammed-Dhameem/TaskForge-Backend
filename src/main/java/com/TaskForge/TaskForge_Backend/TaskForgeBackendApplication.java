b948b2c32b931db7bdd072de0949bb980cb29d46
Mohamme-Dhameem <nmaldhameem115@gmail.com>
2026-02-21 12:33:21 +0530

    Initial Comite

---
+++ src/main/java/com/TaskForge/TaskForge_Backend/TaskForgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TaskForge.TaskForge_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskForgeBackendApplication.class, args);
+	}
+
+}
